eb6216bb7b45a922ed10583f61a54ac5400baad5
Felix Stupp <felix.stupp@banananet.work>
2025-10-04 22:11:32 +0000

    replace substituteAll with replaceVarsWith
    required for upgrading to 25.11

--- package.nix
+++ package.nix
@@ -1,7 +1,7 @@
 {
   lib,
   # build environment helpers
-  substituteAll,
+  replaceVarsWith,
   installShellFiles,
   # script dependencies
   bash,
@@ -17,28 +17,32 @@
 let
   inherit (lib.strings) makeBinPath;
   inherit (lib.meta) getExe;
-in
-substituteAll {
   name = "disko-install-menu";
+in
+replaceVarsWith {
+  inherit name;
 
   src = ./setup.py;
   dir = "bin";
   isExecutable = true;
 
   # referred to by ./setup.py via @<var>@ notation
-  runtimePython = getExe python3Minimal;
-  path = makeBinPath [
-    bash
-    disko # for disko-install
-    fzf
-    nix
-    nixos-rebuild
-    nix-output-monitor
-    python3Minimal
-    smartmontools # for smartctl
-    util-linux # for lsblk, fdisk
-  ];
-  hostPreviewNix = ./host-preview.nix;
+  replacements = {
+    inherit name;
+    runtimePython = getExe python3Minimal;
+    path = makeBinPath [
+      bash
+      disko # for disko-install
+      fzf
+      nix
+      nixos-rebuild
+      nix-output-monitor
+      python3Minimal
+      smartmontools # for smartctl
+      util-linux # for lsblk, fdisk
+    ];
+    hostPreviewNix = ./host-preview.nix;
+  };
 
   nativeBuildInputs = [ installShellFiles ];
 
