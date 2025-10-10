732312ed04a18c23afd7cdd2971b79582239de6c
Felix Stupp <felix.stupp@banananet.work>
2025-10-10 16:40:11 +0000

    convert to flake-parts

--- flake.nix
+++ flake.nix
@@ -2,61 +2,56 @@
   description = "Interactive Installer Menu for Flake-based NixOS Disko Configurations";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
+    { nixpkgs, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { self, lib, ... }@top:
+      {
+        systems = [
+          "x86_64-linux"
+        ];
 
-      inherit (builtins) attrNames;
-      inherit (nixpkgs) lib;
-      inherit (lib) genAttrs;
-
-      supportedSystems = attrNames nixpkgs.legacyPackages;
-      systemSpecificVars = system: {
-        pkgs = import nixpkgs { inherit system; };
-        inherit system;
-      };
-      forAllSystems = gen: genAttrs supportedSystems (system: gen (systemSpecificVars system));
-
-    in
-    {
-
-      devShells = forAllSystems (
-        { pkgs, ... }:
-        rec {
-          default = test-config;
-          test-config = pkgs.mkShell {
-            shellHook = ''
-              export CONFIG_PATH=./test_config
-            '';
+        flake = {
+          nixosModules = rec {
+            # with package from flake configured (allowing easier use)
+            default = (
+              { pkgs, ... }:
+              {
+                imports = [ disko-install-menu ];
+                config.programs.disko-install-menu.package = self.packages.${pkgs.system}.disko-install-menu;
+              }
+            );
+            # raw module exported (assuming package being available in system’s pkgs)
+            disko-install-menu = {
+              imports = [ ./module.nix ];
+            };
           };
-        }
-      );
-
-      nixosModules = rec {
-        # with package from flake configured (allowing easier use)
-        default = (
-          { pkgs, ... }:
-          {
-            imports = [ disko-install-menu ];
-            config.programs.disko-install-menu.package = self.packages.${pkgs.system}.disko-install-menu;
-          }
-        );
-        # raw module exported (assuming package being available in system’s pkgs)
-        disko-install-menu = {
-          imports = [ ./module.nix ];
         };
-      };
 
-      packages = forAllSystems (
-        { pkgs, ... }:
-        rec {
-          default = disko-install-menu;
-          disko-install-menu = pkgs.callPackage ./package.nix { };
-        }
-      );
+        perSystem =
+          { pkgs, system, ... }:
+          {
 
-    };
+            devShells = rec {
+              default = test-config;
+              test-config = pkgs.mkShell {
+                shellHook = ''
+                  export CONFIG_PATH=./test_config
+                '';
+              };
+            };
+
+            packages = rec {
+              default = disko-install-menu;
+              disko-install-menu = pkgs.callPackage ./package.nix { };
+            };
+
+          };
+
+      }
+    );
 }
