e45ff15c4464258e50c8b19122e8e11eb011112a
Felix Stupp <felix.stupp@banananet.work>
2025-10-11 19:53:12 +0000

    fix rendering host preview

--- host-preview.nix
+++ host-preview.nix
@@ -1,2 +1,2 @@
 # used by setup itself to retrieve existing description or fall back to generating itself
-{ config, ... }@host: config.system.description or import ./support/host-preview.nix host
+{ config, ... }@host: config.system.description or (import ./support/host-preview.nix host)
